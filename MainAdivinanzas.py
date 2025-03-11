308471cf1819e84c5569f7412a3d7c0cd77370a6
RubenLopezB-Ast <rlb09@hotmail.es>
2025-03-11 18:53:22 +0100

    Ej 1 al 5

--- MainAdivinanzas.py
+++
@@ -1,9 +0,0 @@
-#Escribe un programa que almacene un número y pida al usuario adivinarlo numeros del 1 al 10.
-class adivinanza:
-    numero_secreto = 2
-    adivinanza = int(input("¿Que número estoy pensando: "))
-    if adivinanza == numero_secreto:
-        print("¡Eres un genio es este !")
-    else:
-        print("Prueba otra vez")
-
